c5673db8dffc521cba6a0c7fda5921a4a5185d0d
Aaron <851882108@qq.com>
2021-07-28 14:57:12 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ WORKDIR  /app
 ADD ./ruoyi-admin.jar /app/ruoyi-admin.jar 
 ADD ./run.sh   /app/run.sh
 ADD ./agent   /app/agent
-RUN chmod +x /app/run.sh
+RUN wget https://buildpack.oss-cn-shanghai.aliyuncs.com/jdk/jdk1.8.0.tgz && \
+tar xvf jdk1.8.0.tgz && \
+chmod +x /app/run.sh
 
 EXPOSE 5000
 ENTRYPOINT ["/app/run.sh"]
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 sleep ${PAUSE:-0}
 
-exec java  $JAVA_OPTS -javaagent:/app/agent/skywalking-agent.jar -Dskywalking.agent.service_name=ruoyi -Dskywalking.collector.backend_service=106.15.152.33:27110  -Dfile.encoding=UTF-8   -jar  /app/ruoyi-admin.jar 
+exec /app/jdk1.8.0/bin/java  $JAVA_OPTS -javaagent:/app/agent/skywalking-agent.jar -Dskywalking.agent.service_name=ruoyi -Dskywalking.collector.backend_service=106.15.152.33:27110  -Dfile.encoding=UTF-8   -jar  /app/ruoyi-admin.jar 
